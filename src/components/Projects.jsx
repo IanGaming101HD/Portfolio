6aa51321021de9f8d1301314366858a3875a88d4
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2024-11-27 23:02:58 +0000

    Experimenting with Projects UI

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,10 @@ import image from '../assets/sung-jin-woo.png';
 // import image from '../assets/django.svg';
 
 function Projects() {
+  let handleClick = () => {
+    console.log('hi')
+  }
+
   return (
     <div id='projects-container' className='projects-container section-container'>
       <h2 id='projects' class='section-header'>
@@ -11,39 +15,93 @@ function Projects() {
       <div className='project-boxes'>
         <div className='project-box'>
           <img className='project-image' src={image} alt='test' />
-          <span className='project-header'>Web Development</span>
-          <span className='project-description'>Developed this portfolio, and created many other applications such as a web dashboard for managing Discord Bots, browser extensions and recently created an interactive chess game for my NEA project.</span>
-          <button class='review-project-button'>Review Project</button>
+          <span className='project-header'>Chess</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
         </div>
         <div className='project-box'>
           <img className='project-image' src={image} alt='test' />
-          <span className='project-header'>Desktop Applications</span>
-          <span className='project-description'>Developed numerous desktop applications such as a flashcard revision tool, an autoclicker, and an E-commerce Platform.</span>
-          <button class='review-project-button'>Review Project</button>
+          <span className='project-header'>Space Invaders</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
         </div>
         <div className='project-box'>
           <img className='project-image' src={image} alt='test' />
-          <span className='project-header'>Game Development</span>
-          <span className='project-description'>Including the Chess game, I have created many projects using PyGame, Canvas and other tools such as Pong, Snake and Space Invaders.</span>
-          <button class='review-project-button'>Review Project</button>
+          <span className='project-header'>Discord Bot</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
         </div>
         <div className='project-box'>
           <img className='project-image' src={image} alt='test' />
-          <span className='project-header'>Game Scripting</span>
-          <span className='project-description'>Scripted game mechanics and designed an interactive GUI for players using Roblox, Unity.</span>
-          <button class='review-project-button'>Review Project</button>
+          <span className='project-header'>Pong</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
         </div>
         <div className='project-box'>
           <img className='project-image' src={image} alt='test' />
-          <span className='project-header'>Video Editing</span>
-          <span className='project-description'>I have created included montages, special effects and high quality content, with the use of tools such as Adobe Premiere Pro and Adobe After Effects.</span>
-          <button class='review-project-button'>Review Project</button>
+          <span className='project-header'>Snake</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
         </div>
         <div className='project-box'>
           <img className='project-image' src={image} alt='test' />
-          <span className='project-header'>Databases</span>
-          <span className='project-description'>I have experience working with a variety of databases, ranging from simple spreadsheets and JSON files to more complex systems like SQL. I’ve utilized SQL extensively for tasks such as creating, updating, deleting, searching, and sorting data. Additionally, I have hands-on experience with MongoDB and MySQL, which I’ve used to store and manage information for various projects, including developing Discord Bots.</span>
-          <button class='review-project-button'>Review Project</button>
+          <span className='project-header'>E-commerce platform</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
+        </div>
+        <div className='project-box'>
+          <img className='project-image' src={image} alt='test' />
+          <span className='project-header'>Tab's Saver Browser Extension</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
+        </div>
+        <div className='project-box'>
+          <img className='project-image' src={image} alt='test' />
+          <span className='project-header'>Portfolio</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
+        </div>
+        <div className='project-box'>
+          <img className='project-image' src={image} alt='test' />
+          <span className='project-header'>Full stack website</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
+        </div>
+        <div className='project-box'>
+          <img className='project-image' src={image} alt='test' />
+          <span className='project-header'>Connect 4</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
+        </div>
+        <div className='project-box'>
+          <img className='project-image' src={image} alt='test' />
+          <span className='project-header'>Flag guesser game</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
+        </div>
+        <div className='project-box'>
+          <img className='project-image' src={image} alt='test' />
+          <span className='project-header'>Wordle</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
+        </div>
+        <div className='project-box'>
+          <img className='project-image' src={image} alt='test' />
+          <span className='project-header'>Hangman</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
+        </div>
+        <div className='project-box'>
+          <img className='project-image' src={image} alt='test' />
+          <span className='project-header'>Flashcards revision tool</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
+        </div>
+        <div className='project-box'>
+          <img className='project-image' src={image} alt='test' />
+          <span className='project-header'>TicTacToe</span>
+          <span className='project-description'>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test</span>
+          <button class='review-project-button' onClick={handleClick}>Review Project</button>
         </div>
       </div>
     </div>
